0ac62bfe92044968f60d80ae839f21e1b44a2c5d
CrazyBoy49z <yura.finiv@gmail.com>
2025-06-19 20:45:24 +0300

    Add translation files for SEO module (English and Ukrainian)

---
+++ resources/lang/en/seo.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Title',
+    'description' => 'Description',
+    'keywords' => 'Keywords',
+    'save' => 'Save',
+    'redirects' => 'Redirects',
+    'status_code' => 'Status Code',
+    'old_url' => 'Old URL',
+    'new_url' => 'New URL',
+];
---
+++ resources/lang/uk/seo.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Заголовок',
+    'description' => 'Опис',
+    'keywords' => 'Ключові слова',
+    'save' => 'Зберегти',
+    'redirects' => 'Редіректи',
+    'status_code' => 'Код статусу',
+    'old_url' => 'Старий URL',
+    'new_url' => 'Новий URL',
+];
