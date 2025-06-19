e5f9e93cb03b216a937f7401172f615b2e93dde8
CrazyBoy49z <yura.finiv@gmail.com>
2025-06-19 20:36:20 +0300

    Add `JsonLdService` for generating structured JSON-LD for web pages

---
+++ src/Services/JsonLdService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Step2dev\LazySeoTools\Services;
+
+class JsonLdService
+{
+    public function generateForPage(array $data): string
+    {
+        $json = [
+            '@context' => 'https://schema.org',
+            '@type' => 'WebPage',
+            'name' => $data['title'] ?? config('app.name'),
+            'description' => $data['description'] ?? '',
+            'url' => $data['url'] ?? request()->fullUrl(),
+        ];
+
+        return '<script type="application/ld+json">' . json_encode($json, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . '</script>';
+    }
+}
